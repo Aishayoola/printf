6601bf0b65dc68af451c1339908c64c7ec8a3178
Emmanuel0846 <olaoluwaidowu1666@gmail.com>
2023-03-28 17:12:32 -0700

    printf new update

--- functions.c
+++ functions.c
@@ -141,10 +141,10 @@ int print_int(va_list types, char buffer[],
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
-/************************* PRINT BINARY ************************/
+/************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
- * @types: List of arguments
+ * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
@@ -155,24 +155,34 @@ int print_int(va_list types, char buffer[],
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int n = va_arg(types, int);
-	int i;
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
 
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	for (i = sizeof(int) * 8 - 1; i >= 0; i--)
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
 	{
-		if (n & (1 << i))
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
 		{
-			putchar('1');
-		}
-		else
-		{
-			putchar('0');
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
 		}
 	}
-	return (0);
+	return (count);
 }
